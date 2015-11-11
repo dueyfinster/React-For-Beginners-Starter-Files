620a6c5a90921908f3c91c63122f3ec8dca5786c
Neil Grogan <neil@ngrogan.com>
2015-11-11 15:51:38 +0000

    Finished 4th Video

--- 01 - Introduction - Start Here/scripts/main.js
+++ 01 - Introduction - Start Here/scripts/main.js
@@ -10,7 +10,11 @@ var ReactDOM = require('react-dom');
 var StorePicker = React.createClass({
     render : function() {
         return (
-            <p>hello</p>
+            <form className="store-selector">
+            <h2>Please Enter a Store</h2>
+            <input type="text" ref="storeId" required />
+            <input type="submit" />
+            </form>
             )
     }
 
