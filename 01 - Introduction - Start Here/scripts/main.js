936e3da152332d0b997e3b1f5e5075e3fe26069c
Neil Grogan <neil@ngrogan.com>
2015-11-11 15:45:06 +0000

    Finished 3rd Video

---
+++ 01 - Introduction - Start Here/scripts/main.js
@@ -0,0 +1,19 @@
+//alert("hi");
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+/*
+  StorePicker
+  This will let us make StorePicker
+*/
+
+var StorePicker = React.createClass({
+    render : function() {
+        return (
+            <p>hello</p>
+            )
+    }
+
+});
+
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'))
